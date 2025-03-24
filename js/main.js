fd629361eda71ca9cb9b60cd013c148035ba008f
wyh <2668590764@qq.com>
2025-03-24 20:19:32 +0800

    first

--- js/main.js
+++ js/main.js
@@ -1,64 +1,171 @@
 var tempFontSize = 200,
 	tempDotRadius = 4,
 	tempDotDistance = 10;
-function getProperFont (length) {
+function getProperFont(length) {
 	var windowWidth = getWindowWidth();
 	var windowHeight = getWindowHeight();
-	tempFontSize = (windowWidth)/(length-6) >= 200 ? 200 : (windowWidth)/(length-6);
-	if(tempFontSize < 80){
+	tempFontSize = (windowWidth) / (length - 6) >= 200 ? 200 : (windowWidth) / (length - 6);
+	if (tempFontSize < 80) {
 		tempDotRadius = 1;
-	}else if(tempFontSize < 100){
+	} else if (tempFontSize < 100) {
 		tempDotRadius = 2;
-	}else if(tempFontSize < 150){
+	} else if (tempFontSize < 150) {
 		tempDotRadius = 3;
-	}else{
+	} else {
 		tempDotRadius = 4;
 	}
-	tempDotDistance = tempDotRadius*2+2;
+	tempDotDistance = tempDotRadius * 2 + 2;
 
 }
-function getWindowWidth () {
+function getWindowWidth() {
 	return document.body.clientWidth;
 }
-function getWindowHeight () {
+function getWindowHeight() {
 	return document.body.clientHeight;
 }
-getProperFont(14);
-var pc = new particleChar({fontColor:"#76A4D4",fontSize:tempFontSize,dotRadius:tempDotRadius,dotDistance:tempDotDistance});
+getProperFont(18);
+var pc = new particleChar({ fontColor: "#76A4D4", fontSize: tempFontSize, dotRadius: tempDotRadius, dotDistance: tempDotDistance });
 pc.queueCreate({
-	text:"particleChar.js",
+	text: "亲爱的曹操",
 	showTypeAfter: "top",
 	showTime: 1800,
 	v1: 0.15,
 	waitTime: 0,
-	callbackBefore: function  () {
+	callbackBefore: function () {
 		document.body.scrollTop = 0;
 	},
-},{
-	text: "IwYvI",
+}, {
+	text: "一直以来",
 	showTypeBefore: "top",
 	showOpen: false,
 	fontColor: "#95f0c0",
 	showTime: 1200,
-	callbackAfter: function  () {
-		document.getElementById('container').style.top = "-" + getWindowHeight()/2 + "px";
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
 	}
-},{
-	text: "particleChar.js",
+}, {
+	text: "遇到困难时",
+	showTypeBefore: "top",
 	showOpen: false,
-	fontColor: "#ff6268",
-	yOffset: -0.5,
-	showNext: false,
-	showTypeBefore: "nearby",
-	callbackBefore: function  () {
-		document.body.style.overflow = "auto";
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
 	}
-});
-window.onresize = function  () {
-	getProperFont(pc.getText().length);
-	pc.setOption({fontSize:tempFontSize,dotRadius:tempDotRadius,dotDistance:tempDotDistance});
-	pc.repaint();
-	if(document.body.style.overflow == "auto"){
-		document.getElementById('container').style.top = "-" + getWindowHeight()/2 + "px";
+}, {
+	text: "你总是陪在我身边",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
 	}
-};
+}, {
+	text: "开导我，安慰我",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "时常觉得你就像一个大姐姐",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "总是温暖着我",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "不知不觉",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "我们已经一起走过这么多年",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "我们以后一定",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "还能一直陪伴在彼此身边",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "二十岁，不管有多么迷茫",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "都有我陪在你身边",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "生日快乐！",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#76A4D4",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+}, {
+	text: "爱你！",
+	showTypeBefore: "top",
+	showOpen: false,
+	fontColor: "#95f0c0",
+	showTime: 1200,
+	callbackAfter: function () {
+		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+	}
+})
+// window.onresize = function () {
+// 	getProperFont(pc.getText().length);
+// 	pc.setOption({ fontSize: tempFontSize, dotRadius: tempDotRadius, dotDistance: tempDotDistance });
+// 	pc.repaint();
+// 	if (document.body.style.overflow == "auto") {
+// 		document.getElementById('container').style.top = "-" + getWindowHeight() / 2 + "px";
+// 	}
+// };
